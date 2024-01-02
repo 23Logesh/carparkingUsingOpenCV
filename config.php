4deae7bde2441f73af8c4f55fd5155275e64bc61
Logeshwaran <140958412+23Logesh@users.noreply.github.com>
2024-01-02 23:02:37 +0530

    first commit

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 session_start();
-define('DB_HOST', 'sql302.infinityfree.com');
+define('DB_HOST', 'sql12.freesqldatabase.com');
 define('DB_PORT', 3306);
-define('DB_USER', 'if0_35708405');
+define('DB_USER', 'sql12673883');
 define('DB_PASSWORD', 'Kirthika23');
-define('DB_NAME', 'if0_35708405_moonscarparkingDB');
+define('DB_NAME', 'sql12673883');
 
 
 // Establish a MySQL database connection
